d60af47149c2b79700a7afea85e4cdb6e77ae929
Student <student@M303-28.local>
2017-01-21 12:02:24 +0900

    kari

--- test/ViewController.m
+++ test/ViewController.m
@@ -7,21 +7,27 @@
 //
 
 #import "ViewController.h"
-
+#import "ACEDrawingView.h"
 @interface ViewController ()
-
+{IBOutlet ACEDrawingView* myDrawingView;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    myDrawingView.lineColor = [UIColor redColor];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+    
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
---
+++ test/clear.swift
@@ -0,0 +1,11 @@
+//
+//  clear.swift
+//  test
+//
+//  Created by Student on 2017/01/21.
+//  Copyright © 2017年 Student. All rights reserved.
+//
+
+import Foundation
+// 全てを削除します
+[self.drawing View clear];
